0843ee4994da2b2ff86bd957b61a65f1e1fe0efa
jiangxubin <21612149@zju.edu.cn>
2018-09-29 16:29:36 +0800

    Finish bst_min_diff

---
+++ Interview/LeetCode.cpp
@@ -0,0 +1,61 @@
+﻿#include "stdafx.h"
+#include "LeetCode.h"
+
+
+LeetCode::LeetCode()
+{
+}
+
+
+LeetCode::~LeetCode()
+{
+}
+
+string LeetCode::decode_string(string s)
+{
+	stack<int> nums;
+	stack<string> chars;
+	string res;
+	int num=0;
+	for (char c : s) {
+		if (isdigit(c)) {
+			num = num*10+(c-'0');
+		}
+		else if (c == '[') {
+			chars.push(res);
+			nums.push(num);
+			num = 0;
+			res = "";
+		}
+		else if (isalpha(c)) {
+			res += c;
+		}
+		else if (c == ']') {
+			string temp = res;
+			for (int i = 0; i < nums.top() - 1; i++) {
+				res += temp;
+			}
+			res = chars.top() + res;
+			chars.pop();
+			nums.pop();
+		}
+	}
+	return res;
+}
+
+int LeetCode::bst_min_diff(Node * root)
+{
+	int pre=numeric_limits<int>::min(), result=numeric_limits<int>::max();
+	auxiliary(root, pre, result);
+	return result;
+}
+
+void LeetCode::auxiliary(Node *root, int &pre, int &res) {
+	if (!root) return;
+	auxiliary(root->left, pre, res);
+	if (pre != -1) {
+		res = min(res, root->val-pre);
+	}
+	pre = root->val;
+	auxiliary(root->right, pre, res);
+}
---
+++ Interview/LeetCode.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include <string>
+#include <stack>
+#include <algorithm>
+#include <limits>
+#include "ctype.h"
+
+using namespace std;
+
+
+class LeetCode
+{
+public:
+	struct Node {
+		int val;
+		Node *left;
+		Node *right;
+		Node(int x) :val(x), left(NULL), right(NULL) {};
+	};
+	LeetCode();
+	~LeetCode();
+	string decode_string(string s);
+	int bst_min_diff(Node *root);
+	void auxiliary(Node *root, int &pre, int &res);
+	//https://leetcode.com/problems/minimum-distance-between-bst-nodes/discuss/128516/C++-Recursive-and-Iterative-Summary-for-Question-783-and-530.
+	//http://www.cnblogs.com/grandyang/p/6540165.html
+	//http://www.cnblogs.com/grandyang/p/6540165.html
+};
+
